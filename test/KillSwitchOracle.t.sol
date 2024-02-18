5a619491718c331232a87242fe8bc7a0d50ea7c1
Sam MacPherson <xylom44@gmail.com>
2024-02-18 11:19:31 -0300

    finish unit tests

--- test/KillSwitchOracle.t.sol
+++ test/KillSwitchOracle.t.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-import { IPool }             from "lib/aave-v3-core/contracts/interfaces/IPool.sol";
-import { IPoolConfigurator } from "lib/aave-v3-core/contracts/interfaces/IPoolConfigurator.sol";
+import { IPool }                from "lib/aave-v3-core/contracts/interfaces/IPool.sol";
+import { IPoolConfigurator }    from "lib/aave-v3-core/contracts/interfaces/IPoolConfigurator.sol";
+import { ReserveConfiguration } from "lib/aave-v3-core/contracts/protocol/libraries/configuration/ReserveConfiguration.sol";
+import { DataTypes }            from "lib/aave-v3-core/contracts/protocol/libraries/types/DataTypes.sol";
 
 import { MockOracle }             from "test/mocks/MockOracle.sol";
 import { MockPool }             from "test/mocks/MockPool.sol";
@@ -13,6 +15,15 @@ import { MockPoolConfigurator } from "test/mocks/MockPoolConfigurator.sol";
 import { KillSwitchOracle } from "src/KillSwitchOracle.sol";
 
 contract KillSwitchOracleTest is Test {
+
+    using ReserveConfiguration for DataTypes.ReserveConfigurationMap;
+
+    event SetOracle(address indexed oracle, uint256 threshold);
+    event DisableOracle(address indexed oracle);
+    event Trigger(address indexed oracle, uint256 threshold, uint256 price);
+    event AssetLTV0(address indexed asset);
+    event AssetFrozen(address indexed asset);
+    event Reset();
 
     MockPool             pool;
     MockPoolConfigurator poolConfigurator;
@@ -23,6 +34,11 @@ contract KillSwitchOracleTest is Test {
 
     address owner         = makeAddr("owner");
     address randomAddress = makeAddr("randomAddress");
+
+    address asset1 = makeAddr("asset1");
+    address asset2 = makeAddr("asset2");
+    address asset3 = makeAddr("asset3");
+    address asset4 = makeAddr("asset4");
 
     function setUp() public {
         pool             = new MockPool();
@@ -55,6 +71,8 @@ contract KillSwitchOracleTest is Test {
         assertEq(killSwitchOracle.numOracles(),                      0);
         assertEq(killSwitchOracle.oracleThresholds(address(oracle)), 0);
 
+        vm.expectEmit(address(killSwitchOracle));
+        emit SetOracle(address(oracle), 0.99e8);
         vm.prank(owner);
         killSwitchOracle.setOracle(address(oracle), 0.99e8);
 
@@ -103,6 +121,8 @@ contract KillSwitchOracleTest is Test {
         assertEq(killSwitchOracle.oracleAt(0),                       address(oracle));
         assertEq(killSwitchOracle.oracleThresholds(address(oracle)), 0.99e8);
 
+        vm.expectEmit(address(killSwitchOracle));
+        emit DisableOracle(address(oracle));
         vm.prank(owner);
         killSwitchOracle.disableOracle(address(oracle));
 
@@ -129,6 +149,8 @@ contract KillSwitchOracleTest is Test {
         
         assertEq(killSwitchOracle.triggered(), true);
         
+        vm.expectEmit(address(killSwitchOracle));
+        emit Reset();
         vm.prank(owner);
         killSwitchOracle.reset();
 
@@ -206,6 +228,144 @@ contract KillSwitchOracleTest is Test {
 
         vm.expectRevert("KillSwitchOracle/price-above-threshold");
         killSwitchOracle.trigger(address(oracle));
+    }
+
+    function test_trigger() public {
+        // Collateral asset (Ex. ETH, wstETH, sDAI, etc)
+        _initReserve({
+            asset: asset1,
+            active: true,
+            frozen: false,
+            paused: false,
+            ltv: 80_00,
+            liquidationThreshold: 83_00,
+            liquidationBonus: 105_00
+        });
+
+        // Borrow-only asset (Ex. DAI, USDC, etc)
+        _initReserve({
+            asset: asset2,
+            active: true,
+            frozen: false,
+            paused: false,
+            ltv: 0,
+            liquidationThreshold: 0,
+            liquidationBonus: 0
+        });
+
+        // Frozen/LTV0 asset (Ex. GNO)
+        _initReserve({
+            asset: asset3,
+            active: true,
+            frozen: true,
+            paused: false,
+            ltv: 0,
+            liquidationThreshold: 25_00,
+            liquidationBonus: 110_00
+        });
+
+        // Paused asset
+        _initReserve({
+            asset: asset4,
+            active: true,
+            frozen: false,
+            paused: true,
+            ltv: 80_00,
+            liquidationThreshold: 83_00,
+            liquidationBonus: 105_00
+        });
+
+        assertEq(pool.getReservesList().length, 4);
+
+        vm.prank(owner);
+        killSwitchOracle.setOracle(address(oracle), 0.99e8);
+        oracle.setLatestAnswer(0.98e8);
+
+        vm.expectEmit(address(killSwitchOracle));
+        emit Trigger(address(oracle), 0.99e8, 0.98e8);
+        vm.expectEmit(address(killSwitchOracle));
+        emit AssetLTV0(asset1);
+        vm.expectEmit(address(killSwitchOracle));
+        emit AssetFrozen(asset2);
+        vm.prank(randomAddress);  // Permissionless call
+        killSwitchOracle.trigger(address(oracle));
+
+        _assertReserve({
+            asset: asset1,
+            active: true,
+            frozen: false,
+            paused: false,
+            ltv: 0,
+            liquidationThreshold: 83_00,
+            liquidationBonus: 105_00
+        });
+
+        _assertReserve({
+            asset: asset2,
+            active: true,
+            frozen: true,
+            paused: false,
+            ltv: 0,
+            liquidationThreshold: 0,
+            liquidationBonus: 0
+        });
+
+        _assertReserve({
+            asset: asset3,
+            active: true,
+            frozen: true,
+            paused: false,
+            ltv: 0,
+            liquidationThreshold: 25_00,
+            liquidationBonus: 110_00
+        });
+
+        _assertReserve({
+            asset: asset4,
+            active: true,
+            frozen: false,
+            paused: true,
+            ltv: 80_00,
+            liquidationThreshold: 83_00,
+            liquidationBonus: 105_00
+        });
+    }
+
+    function _initReserve(
+        address asset,
+        bool active,
+        bool frozen,
+        bool paused,
+        uint256 ltv,
+        uint256 liquidationThreshold,
+        uint256 liquidationBonus
+    ) internal {
+        DataTypes.ReserveConfigurationMap memory configuration = pool.getConfiguration(asset);
+        configuration.setActive(active);
+        configuration.setFrozen(frozen);
+        configuration.setPaused(paused);
+        configuration.setLtv(ltv);
+        configuration.setLiquidationThreshold(liquidationThreshold);
+        configuration.setLiquidationBonus(liquidationBonus);
+        pool.__addReserve(asset, configuration);
+    }
+
+    function _assertReserve(
+        address asset,
+        bool active,
+        bool frozen,
+        bool paused,
+        uint256 ltv,
+        uint256 liquidationThreshold,
+        uint256 liquidationBonus
+    ) internal {
+        DataTypes.ReserveConfigurationMap memory configuration = pool.getConfiguration(asset);
+        assertEq(configuration.getActive(),               active);
+        assertEq(configuration.getFrozen(),               frozen);
+        assertEq(configuration.getPaused(),               paused);
+        assertEq(configuration.getLtv(),                  ltv);
+        assertEq(configuration.getLiquidationThreshold(), liquidationThreshold);
+        assertEq(configuration.getLiquidationBonus(),     liquidationBonus);
     }
     
 }
